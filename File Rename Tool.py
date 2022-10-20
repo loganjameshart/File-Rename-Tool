d93a19a8256247df1d88db80f7cdad1d4b9b33cb
loganjameshart <116290186+loganjameshart@users.noreply.github.com>
2022-10-20 10:02:42 -0500

    Add files via upload

---
+++ File Rename Tool.py
@@ -0,0 +1,23 @@
+import os, csv
+
+#get current directory
+directory = input(r"Please input directory.")
+os.chdir(directory)
+
+#get CSV file you're using
+fileName = input("What is the filename?")
+
+#create CSV object, get data as list
+with open(fileName, "r") as fileObject:
+    fileReader = csv.reader(fileObject)
+    fileData = list(fileReader)
+    print(fileData)
+
+#iterate over the two columns 
+    for column1, column2 in fileData:
+        os.rename(directory+"\\"+column1, directory+"\\"+column2)
+
+print("Renaming complete.")
+
+
+
